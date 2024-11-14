8dc666d5bfd69f428f80fb23d1d166b8302dbfc6
Thibault LONGUEPEE <thibault.longuepee@epitech.eu>
2024-11-14 13:35:14 +0100

    init: setup typescript package

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Adds two numbers: a and b
+ * @param a The first number in the addition
+ * @param b The second number in the addition
+ * @returns The result of a + b
+ */
+export const add = (a: number, b: number) => a + b;

fe4c506e791a2ebba419ea7c54ff92d311c447e9
Thibault <thibault.longuepee@epitech.eu>
2024-12-19 16:50:30 +0100

    feat(engine): add ladder method do targetted property changes

--- src/engine.ts
+++ src/engine.ts
@@ -8,6 +8,15 @@
 import { TeloConfig } from "./config";
 import { EloRatedObject } from "./elo-rated";
 
+function isNumber(x: any): x is number {
+  return typeof x === "number";
+}
+
+type NumericKeys<T> = {
+  [K in keyof T]: T[K] extends number ? K : never;
+}[keyof T];
+
+
 export enum GameResult {
   WIN = 1.0,
   DRAW = 0.5,
@@ -22,10 +31,9 @@ export class Telo {
   }
 
   public static resolve(a: EloRatedObject, b: EloRatedObject, result: GameResult): void {
-    const p_a_win = this._p(a.elo - b.elo);
-    const p_b_win = this._p(b.elo - a.elo);
-    this._updateElo(a, result, p_a_win);
-    this._updateElo(b, 1 - result, p_a_win);
+    const { deltaA, deltaB } = this._getEloChanges(a.elo, b.elo, result);
+    a.elo += deltaA;
+    b.elo += deltaB;
   }
 
   public static player(player: EloRatedObject) {
@@ -42,12 +50,48 @@ export class Telo {
     }
   }
 
-  private static _updateElo(obj: EloRatedObject, w: number, p: number): void {
-    obj.elo += Math.round(this.config.multiplier * (w - p));
+  public static ladder<T>(key: NumericKeys<T>) {
+    return new class {
+      public resolve(a: T, b: T, result: GameResult) {
+        if (!isNumber(a[key]) || !isNumber(b[key]))
+          return;
+        const { deltaA, deltaB } = Telo._getEloChanges(a[key], b[key], result);
+        a[key] = (a[key] + deltaA) as T[NumericKeys<T>];
+        b[key] = (b[key] + deltaB) as T[NumericKeys<T>];
+      }
+      public player(player: T) {
+        return new class {
+          public wonAgainst = (opponent: T) => this._resolve(player, opponent, GameResult.WIN);
+          public lostAgainst = (opponent: T) => this._resolve(player, opponent, GameResult.LOSS);
+          public tiedAgainst = (opponent: T) => this._resolve(player, opponent, GameResult.DRAW);
+          private _resolve(a: T, b: T, result: GameResult) {
+            if (!isNumber(a[key]) || !isNumber(b[key]))
+              return;
+            const { deltaA, deltaB } = Telo._getEloChanges(a[key], b[key], result);
+            a[key] = (a[key] + deltaA) as T[NumericKeys<T>];
+            b[key] = (b[key] + deltaB) as T[NumericKeys<T>];
+          }
+        }
+      }
+    }
+  }
+
+  private static _getEloChanges(eloA: number, eloB: number, result: GameResult): { deltaA: number, deltaB: number } {
+    const pA = this._p(eloA - eloB);
+    const pB = this._p(eloB - eloA);
+    return {
+      deltaA: this._computeEloChange(result, pA),
+      deltaB: this._computeEloChange(1 - result, pB),
+    }
+  }
+
+  private static _computeEloChange(w: number, p: number): number {
+    return Math.round(this.config.multiplier * (w - p));
   }
 
   private static _p(D: number): number {
     D = Math.max(-400, Math.min(D, 400))
     return 1.0 / (1.0 + Math.exp(-D / 180))
   }
+
 }
